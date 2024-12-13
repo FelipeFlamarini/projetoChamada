32c5962a8690883e3dc847ff6084966be20b90c2
nicholasss0 <nicholas.araujo@estudante.ifms.edu.br>
2024-12-13 18:01:25 -0400

    container do deepface

--- fastapi-app/app.py
+++ fastapi-app/app.py
@@ -11,6 +11,9 @@ from api.routers.facial_recognition import facial_recognition_router
 
 from api.schemas.user import UserRead, UserCreate, UserUpdate
 
+import requests
+from fastapi.responses import JSONResponse
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -55,14 +58,29 @@ app.include_router(
 @app.get("/")
 def teste():
  
-    res = DeepFace.verify(
-        img1_path = "./public/teste.jpeg",
-        img2_path = "./public/images/Felipe Flamarini.jpg",
-        enforce_detection=False,
-        model_name = "ArcFace"
-            )
+    img1_path = "./public/images/dataset/img1.jpg"
+    img1_path = "./public/images/dataset/img4.jpg"
 
-    return res
+    deepface_api_url = "http://0.0.0.0:5000/verify"
+
+    with open(img1_path, "rb") as img1, open(img2_path, "rb") as img2:
+        files = {
+            "img1": img1,
+            "img2": img2,
+        }     
+
+    try:
+        response = resquest.post(deepface_api_url, files=files)
+        response.raise_for_status
+        data = response.json()
+
+    except requests.exceptions.RequestException as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": "Failed to connect to DeepFace API", "details": str(e)},
+        )
+
+    return data
 
 @app.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
