cf25536967a013e4618bacd9057f0f8bed0e5f23
kauan345developer <kauanolival@gmail.com>
2024-12-14 23:37:01 -0300

    arrumando layout

--- client/src/components/camera3.tsx
+++ client/src/components/camera3.tsx
@@ -1,11 +1,11 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 import Webcam from "react-webcam";
 
 const FaceDetection = () => {
   const videoRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  // const [width, setWidth] = useState<number>(100);
+  const [size, setSize] = useState({ width: 0, height: 0 });
 
   // Carregar os modelos necessários
   const loadModels = async () => {
@@ -15,14 +15,12 @@ const FaceDetection = () => {
 
   // Iniciar a detecção facial
   const handleVideoPlay = async () => {
-
-    await loadModels()
+    await loadModels();
 
     const video = videoRef.current?.video;
     const canvas = canvasRef.current;
 
     // console.log(video.videoWidth,video.videoHeight)
-    
 
     if (!canvas || !video) return;
 
@@ -33,15 +31,21 @@ const FaceDetection = () => {
       height: video.videoHeight,
     };
 
+    // const displaySize = {
+    //   width: 400,
+    //   height: 400,
+    // };
+
     if (displaySize.width === 0 || displaySize.height === 0) {
       console.error("Invalid video dimensions:", displaySize);
       return;
     }
 
-    faceapi.matchDimensions(canvas, displaySize);
+    faceapi.matchDimensions(canvas, displaySize, true);
 
     setInterval(async () => {
       // console.log("AAAAAA")
+      // setSize({ width: video.videoWidth, height: video.videoHeight });
       const detections = await faceapi.detectSingleFace(
         video,
         new faceapi.TinyFaceDetectorOptions()
@@ -59,13 +63,20 @@ const FaceDetection = () => {
         if (videoRef.current) {
           const imageSrc = videoRef.current.getScreenshot();
           if (imageSrc) {
-            // console.log(imageSrc, new Date().getTime());
-            // console.log("Base64 image:", imageSrc);
+            // fetch("http://localhost:5000/api/face", {
+            //   method: "POST",
+            //   headers: {
+            //     "Content-Type": "application/json",
+            //   },
+            //   body: JSON.stringify({
+            //     image: imageSrc,
+            //   }),
+            // })
           }
         }
 
         const context = canvas.getContext("2d");
-        if (context && canvas.width != 0 && canvas.height != 0 ) {
+        if (context && canvas.width != 0 && canvas.height != 0) {
           context.clearRect(0, 0, canvas.width, canvas.height);
           faceapi.draw.drawDetections(canvas, resizedDetections);
         }
@@ -73,24 +84,33 @@ const FaceDetection = () => {
     }, 650);
   };
 
-
-
   return (
-    <div className="flex justify-center relative">
-      <Webcam
-        ref={videoRef}
-        // autoPlay
-        muted
-        className="border border-btnGo rounded-3xl"
-        // style={{ transform: "scaleX(-1)" }} // Espelhar o vídeo
-        screenshotFormat="image/jpeg"
-        onUserMedia={handleVideoPlay}
-      />
-      <canvas
-        ref={canvasRef}
-        className="absolute w-96 h-96 sm:h-auto sm:w-auto"
-        // style={{ transform: "scaleX(-1)" }} // Espelhar o canvas para corresponder ao vídeo
-      />
+    <div className="flex justify-center">
+      <div className="flex items-start">
+        <Webcam
+          ref={videoRef}
+          // autoPlay
+          muted
+          className="border border-btnGo rounded-3xl"
+          // style={{ transform: "scaleX(-1)" }} // Espelhar o vídeo
+          screenshotFormat="image/jpeg"
+          onUserMedia={handleVideoPlay}
+          // height={400}
+          // width={400}
+        />
+        <canvas
+          ref={canvasRef}
+          className="absolute"
+          // style={{ transform: "scaleX(-1)" }} // Espelhar o canvas para corresponder ao vídeo
+          // width={size.width}
+          // height={size.height}
+        />
+        {/* <Button
+      onClick={handleVideoPlay}
+      >
+        clic
+      </Button> */}
+      </div>
     </div>
   );
 };
--- client/src/components/card-Home.tsx
+++ client/src/components/card-Home.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -11,31 +12,31 @@ import { Link } from "react-router";
 
 export function CardHome() {
   return (
-    <Card className="mx-auto max-w-sm w-full">
+    <Card className="mx-auto max-w-sm w-full border-none flex flex-col flex-1 justify-between">
       <CardHeader className="flex flex-col items-center">
         <CardTitle className="text-2xl">Seja Bem Vindo!</CardTitle>
         <CardDescription className="text-subText text-center">
           O que você deseja fazer agora?
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
+      <CardContent className="flex-1 items-start">
+        <div className="flex flex-col justify-between gap-4 h-full">
           <div className="grid gap-3.5">
             <Button variant={"go"} className="rounded-full" asChild>
               <Link to="/camera">Iniciar Chamada Inteligente</Link>
             </Button>
             <Button variant={"goSecondary"} className="rounded-full">
-              Exportar Lista de Chamafa
+              Exportar Lista de Chamada
             </Button>
             <Button variant={"goOutline"} className="rounded-full" asChild>
               <Link to="/login">Sair</Link>
             </Button>
           </div>
-          <div className="flex justify-center">
-            <img src={undrawHello} alt="" className="w-40 h-w-40" />
-          </div>
         </div>
       </CardContent>
+      <CardFooter className="flex justify-center">
+          <img src={undrawHello} alt="" className="w-40 h-w-40" />
+      </CardFooter>
     </Card>
   );
 }
