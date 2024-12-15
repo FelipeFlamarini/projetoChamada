cba50dc551d40605a62894982af62ef18153ef89
PsSave <pedrosviki1265@gmail.com>
2024-12-14 21:54:46 -0400

    feat: Added database facial verification logic

--- fastapi-app/api/schemas/verify.py
+++ fastapi-app/api/schemas/verify.py
@@ -3,7 +3,13 @@ from pydantic import BaseModel
 class DeepFaceVerifyRequest(BaseModel):
     img1_path: str
     enforce_detection: bool = True
-    model_name: str = "VGG-Face"
+    model_name: str = "SFace"
+
+class DeepFaceStudentReturn(BaseModel):
+    _id: int
+    name: str
+    ra: int
 
 class DeepFaceVerifyReturn(BaseModel):
     verified: bool
+    student: DeepFaceStudentReturn = None
