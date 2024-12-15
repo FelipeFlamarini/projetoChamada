8f6fb249250a2a9ee58d716dae02c6f57bcc4490
Felipe Flamarini <felipeflamarini@hotmail.com>
2024-12-14 23:16:06 -0300

    feat [back-end]: create attendance on verified

--- fastapi-app/api/repositories/facial_recognition.py
+++ fastapi-app/api/repositories/facial_recognition.py
@@ -1,5 +1,7 @@
 import requests
 from api.repositories.Students import StudentsRepository
+from api.repositories.Attendances import AttendancesRepository
+
 
 class FacialRecognitionRepository:
     def recognize(image):
@@ -10,26 +12,42 @@ class FacialRecognitionRepository:
             payload = request.model_dump()
             students = await StudentsRepository.get_all_students()
             for student in students:
-                payload["img2_path"] = '/public/' + str(student.image_path)
-                response = requests.post("http://deepface:5000/verify", json=payload, headers={"Content-Type": "application/json"})
+                print(student.name)
+                payload["img2_path"] = "/public/" + str(student.image_path)
+                response = requests.post(
+                    "http://deepface:5000/verify",
+                    json=payload,
+                    headers={"Content-Type": "application/json"},
+                )
                 if response.json().get("verified"):
+                    await AttendancesRepository.create_attendance(student.ra)
                     return {"verified": True, "student": student.model_dump()}
 
             return {"verified": False}
         except Exception as e:
             return {"error": str(e)}
-        
+
     def represent(request):
         try:
             payload = request.model_dump()
-            response = requests.post("http://deepface:5000/represent", json=payload, headers={"Content-Type": "application/json"})
+            response = requests.post(
+                "http://deepface:5000/represent",
+                json=payload,
+                headers={"Content-Type": "application/json"},
+            )
 
             if response.status_code != 200:
-                return {"error": "Erro ao processar a imagem no DeepFace", "details": response.text}
-            
+                return {
+                    "error": "Erro ao processar a imagem no DeepFace",
+                    "details": response.text,
+                }
+
             response_data = response.json()
             filtered_results = [
-                {"embedding": item["embedding"], "face_confidence": item["face_confidence"]}
+                {
+                    "embedding": item["embedding"],
+                    "face_confidence": item["face_confidence"],
+                }
                 for item in response_data.get("results", [])
             ]
 
