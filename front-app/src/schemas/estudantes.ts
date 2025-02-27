6b42d5309172be4696b464d4ce20762868944d6f
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-02-27 12:26:08 -0300

    fix [back-end and front-end]: enforce positive RA value and improve validation in student schema

--- front-app/src/schemas/estudantes.ts
+++ front-app/src/schemas/estudantes.ts
@@ -1,12 +1,17 @@
 import { z as zod } from "zod";
 
-const base64ImageRegex = /^data:image\/(png|jpeg|jpg|gif|webp);base64,[A-Za-z0-9+/]+={0,2}$/;
+const base64ImageRegex =
+  /^data:image\/(png|jpeg|jpg|gif|webp);base64,[A-Za-z0-9+/]+={0,2}$/;
 
 // Esquema de validação com Zod
 
 const createStudent = zod.object({
-  name: zod.string().min(1,{message:"Nome deve ter pelo menos 1 caractere"}),
-  ra: zod.coerce.number({ message: "RA deve ser um número" }),
+  name: zod
+    .string()
+    .min(1, { message: "Nome deve ter pelo menos 1 caractere" }),
+  ra: zod.coerce
+    .bigint({ message: "RA deve ser um número inteiro" })
+    .positive({ message: "RA deve ser um número positivo" }),
   image_base64: zod.string().regex(base64ImageRegex, "Não é uma imagem valida"),
 });
 
